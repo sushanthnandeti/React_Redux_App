dcd2fcb2b908fdf13e84730ac1a329732ca948af
Sushanth N <sushanthnandeti96@gmail.com>
2024-02-25 02:52:38 -0500

    Use dispatch to delete states from store

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import {useSelector} from 'react-redux';
+import {useDispatch} from 'react-redux';
+import {removeTodo} from "../features/todo/todoslice";
+
 
 function Todos() {
 
   const todos = useSelector(state => state.todos);
-  console.log(todos);
+  const dispatch = useDispatch();
+
 
   return (
     <>
     <div>Todos</div>
     {todos.map((todo) => (
       <li key = {todo.id}> <div> {todo.text} </div>
- 
+      <button onClick = {() => dispatch(removeTodo(todo.id))}> Delete</button>
       </li>
   ))}
     </>
